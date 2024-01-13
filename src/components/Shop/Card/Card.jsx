a8b5459f2ec22df357ac467e7fdc5dbbedb06434
sazzad <sazzad@DESKTOP-1UFS7DJ>
2024-01-14 00:56:50 +0600

    completed card order calculation

--- src/components/Shop/Card/Card.jsx
+++ src/components/Shop/Card/Card.jsx
@@ -4,14 +4,23 @@ import { faArrowRight, faTrash } from '@fortawesome/free-solid-svg-icons';
 
 const Card = (props) => {
     const {cards} = props
+
+    let total = 0;
+    let shipping = 0;
+    for(const product of cards){
+        total = total + product.price
+        shipping = shipping + product.shipping
+    }
+    const tax = (total * 0.1).toFixed(2);
+    const grandTotal = total + shipping + parseFloat(tax);
     return (
         <div className="card-container">
             <h6>Order Summary</h6>
             <p>Selected Items: {cards.length}</p>
-            <p>Total Price: $</p>
-            <p>Total Shipping Charge: $5</p>
-            <p>Tax: $114</p>
-            <p className='total'>Grand Total: $1559</p>
+            <p>Total Price: ${total}</p>
+            <p>Total Shipping Charge: ${shipping}</p>
+            <p>Tax: ${tax}</p>
+            <p className='total'>Grand Total: ${grandTotal.toFixed(2)}</p>
             <div className='btn-div'>
                 <button className='btn-1'><span className='btn-text'>Clear Cart</span> <FontAwesomeIcon icon={faTrash}/></button>
                 <button className='btn-2'><span className='btn-text'>Review Order</span> <FontAwesomeIcon icon={faArrowRight}/></button>
